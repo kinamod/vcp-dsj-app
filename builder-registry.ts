ad986eac95c2174c370319eb3180ed16e460bc45
kinamod <Domanic.Smith-Jones@Auth0.com>
2025-04-25 09:28:17 +0100

    Update builder-registry.ts

--- builder-registry.ts
+++ builder-registry.ts
@@ -12,6 +12,7 @@ import SplitHero from "./components/Hero/SplitHero";
 import TextHero from "./components/Hero/TextHero";
 
 builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
+builder.init('ad14caa2dcd041ed8261eed483ddd361');
 
 Builder.register("editor.settings", {
   styleStrictMode: false,
