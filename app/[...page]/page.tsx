9e605fdcdd6b9aa2d84874a0538578440eb731c7
kinamod <Domanic.Smith-Jones@Auth0.com>
2025-04-28 09:32:10 +0100

    also pays attention to path for app

--- app/[...page]/page.tsx
+++ app/[...page]/page.tsx
@@ -20,7 +20,7 @@ export default async function Page(props: PageProps) {
     .get(builderModelName, {
       userAttributes: {
         // Use the page path specified in the URL to fetch the content
-        urlPath: "/",
+        urlPath: "/" + ((await props?.params)?.page?.join("/") || ""),
       },
       options: {
         locale: localeNow
